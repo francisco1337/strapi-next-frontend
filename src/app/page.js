4544a65899b007dbddf66415cd16dc4503691497
francisco1337 <francisco.sdv.mtz@hotmail.com>
2023-06-07 15:51:47 -0600

    next project

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+
+'use client';
+
+import React from 'react'
+import { Button } from 'semantic-ui-react';
+
+
+
+const Index = () => (
+  <div>
+    <h2>Games shop</h2>
+    <Button primary> botón </Button>
+  </div>
+)
+
+export default Index;
